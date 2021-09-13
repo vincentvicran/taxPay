53a42831219e76e41ba0eb4c95649d59481835dd
vincentvicran <vvicran@gmail.com>
2021-09-13 14:43:20 +0545

    Refactoring for API handling done for users

--- src/sources/userSource.js
+++ src/sources/userSource.js
@@ -1,5 +1,37 @@
 import { request } from '../utils/request';
 
-export const getData = () => {
+export const getUser = () => {
     return request.get('users');
+};
+
+export const getMe = () => {
+    return request.get('users/me');
+};
+
+export const postLogin = (userEmail, userPassword) => {
+    return request.post('users/login', { userEmail, userPassword });
+};
+
+export const postRegister = (userData) => {
+    return request.post('users/register', userData);
+};
+
+export const patchUser = (userData) => {
+    return request.patch('users/updateme', userData);
+};
+
+export const patchPassword = (passwords) => {
+    return request.patch('users/updatepassword', { passwords });
+};
+
+export const getUserOut = () => {
+    return request.get('users/logout');
+};
+
+export const postForgotPassword = (userEmail) => {
+    return request.patch('users/forgotpassword', { userEmail });
+};
+
+export const postResetPassword = (token, passwords) => {
+    return request.patch(`users/resetpassword/${token}`, { passwords });
 };
